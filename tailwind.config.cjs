c15d23dd69fd1b044bee90d5a62da09d45086411
fredericoo <rincofrederico@gmail.com>
2023-06-01 19:14:22 +0100

    feat: font metrics adjust for fallback

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
-      body: ["neue-montreal", "Inter", "sans-serif"],
+      body: ["neue-montreal", "Arial"],
     },
     fontSize: {
       heading: [
