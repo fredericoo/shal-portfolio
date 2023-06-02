d1e53b3898d75e672be3acb59ebacf771d28abb1
fredericoo <rincofrederico@gmail.com>
2023-06-02 16:35:36 +0100

    feat: add vercel pkg

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,15 @@ import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
 import prefetch from "@astrojs/prefetch";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    preact(),
-    mdx(),
-    prefetch({ throttle: 3 }),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [tailwind(), preact(), mdx(), prefetch({
+    throttle: 3
+  }), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })],
+  output: "server",
+  adapter: vercel()
 });
