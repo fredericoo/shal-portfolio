ad918001a69fd7fd73759fc4f94f43e08328e887
freddie <rincofrederico@gmail.com>
2023-07-29 18:20:46 +0100

    feat: content and utils

--- src/components/content/index.ts
+++ src/components/content/index.ts
@@ -1,5 +1,6 @@
+import { Picture } from "@astrojs/image/components";
 import { default as Card } from "./ContentCard.astro";
 import { default as Container } from "./ContentContainer.astro";
 import { default as Grid } from "./ContentGrid.astro";
-
-export { Card, Container, Grid };
+export { default as Prose } from "./ContentProse.astro";
+export { Card, Container, Grid, Picture };
